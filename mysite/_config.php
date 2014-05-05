faf525725062d0c1136e56eca4bfc521aead478b
ethanGmail <ethanleenz@gmail.com>
2014-05-05 16:18:39 +1200

    Initial commit

---
+++ mysite/_config.php
@@ -0,0 +1,17 @@
+<?php
+
+global $project;
+$project = 'mysite';
+
+global $databaseConfig;
+$databaseConfig = array(
+	"type" => 'MySQLDatabase',
+	"server" => 'localhost',
+	"username" => 'root',
+	"password" => 'passw0rd',
+	"database" => 'Kordia_Reinvigorate',
+	"path" => '',
+);
+
+// Set the site locale
+i18n::set_locale('en_GB');
